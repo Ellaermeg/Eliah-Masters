7ddb2aad391eadaedbe809431e527ec1d51c80e2
Ella <eliahmathias@gmail.com>
2024-03-04 15:18:51 +0100

    Fixing pluh

--- K_func.py
+++ K_func.py
@@ -1,14 +1,17 @@
 from bioservices import KEGG
 kegg = KEGG()
 
-def translate_ko_terms(ko_terms):
-    ko_names = {}
+def translate_ko_terms_with_find(ko_terms):
+    ko_descriptions = {}
     for ko in ko_terms:
         try:
-            info = kegg.get(ko)  # Retrieve information for the KO term
-            definition = kegg.parse(info)['DEFINITION']  # Parse the information to get the definition
-            ko_names[ko] = definition.split(';')[0]  # Sometimes definitions contain multiple parts; take the first
+            result = kegg.find("ko", ko)
+            # The result can contain multiple lines if the term has multiple entries; split by newline
+            first_line = result.split('\n')[0]
+            # Each line is tab-separated with the format: entry_id, description
+            _, description = first_line.split('\t', 1)
+            ko_descriptions[ko] = description
         except Exception as e:
             print(f"Error retrieving information for {ko}: {e}")
-            ko_names[ko] = ko  # Use the KO term itself if the name can't be retrieved
-    return ko_names
+            ko_descriptions[ko] = ko  # Use the KO term itself if the name can't be retrieved
+    return ko_descriptions
