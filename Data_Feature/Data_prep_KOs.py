aed0517e98d4fc0eca869be7d463f4a7a293faeb
Ella <eliahmathias@gmail.com>
2024-03-18 17:41:56 +0100

    Fixed errors and bugs- Added classes to dataprep

--- Data_Feature/Data_prep_KOs.py
+++ Data_Feature/Data_prep_KOs.py
@@ -13,102 +13,92 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
 import zipfile
 from bioservices import KEGG
-from K_func import translate_ko_terms
+
+
+class DataProcessor:
+    def __init__(self, terms_zip_path, terms_csv_path, traits_zip_path, traits_csv_path):
+        self.terms_zip_path = terms_zip_path
+        self.terms_csv_path = terms_csv_path
+        self.traits_zip_path = traits_zip_path
+        self.traits_csv_path = traits_csv_path
+
+    def load_data_from_zip(self, zip_path, csv_path):
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            with zip_ref.open(csv_path) as file:
+                data = pd.read_csv(file, index_col=0)
+        return data
+
+    def preprocess_terms(self, terms_data):
+        raise NotImplementedError("Use subclasses, eg: traits_data .")
+
+    def preprocess_traits_oxygen(self, traits_data):
+        raise NotImplementedError("Use subclasses, eg: terms_data")
 
 
 
+class KOProcessor(DataProcessor):
+    def preprocess_terms(self, terms_data):
+
+        terms_data['value'] = 1
+        X_terms = terms_data.pivot_table(index='key', columns='KO', values='value', fill_value=0)
+
+        # Variance threshold for removal of features
+        selector = VarianceThreshold(threshold=0.01)
+        X_filtered = selector.fit_transform(X_terms)
+        X_filtered_df = pd.DataFrame(X_filtered, index=X_terms.index, columns=X_terms.columns[selector.get_support()])
+        return X_filtered_df
+
+    def preprocess_traits_oxygen(self, traits_data):
+        traits_data['oxygen'] = traits_data['oxygen'].str.lower()
+        traits_data['oxygen'] = traits_data['oxygen'].map({
+            'aerobic': 'aerobic',
+            'aerotolerant': 'aerobic',
+            'microaerophilic': 'aerobic',
+            'obligate aerobic': 'aerobic',
+            'anaerobic': 'anaerobic',
+            'obligate anaerobic': 'anaerobic',
+            'conflict': 'aerobic',  
+            'facultative': 'aerobic'  
+        })
+        y = traits_data.dropna(subset=['oxygen']).groupby('key').agg({'oxygen': lambda x: x.value_counts().index[0]})
+        return y
+
+
+class GOProcessor(DataProcessor):
+    def preprocess_terms(self, terms_data):
 
 
 
+        return X_filtered_df
+    def preprocess_traits(self, traits_data):
+        
+        return y
+
+
+class COGsProcessor(DataProcessor):
+    def preprocess_terms(self, terms_data):
+        return X_filtered_df
+    
+    def preprocess_traits(self, traits_data):
+       return y
+    
 
 
 
+# Something like this if i want to make a pipline method
+class ModelPipeline:
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
 
+    def setup_pipeline(self):
+        # Define the pipeline with steps and parameter grid
+        pass
 
+    def train_model(self):
+        # Split data, run GridSearchCV, etc.
+        pass
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-Data prep and feature mapping
-'''
-# Paths to your uploaded files
-zip_file_path_KO = 'Eliah-Masters\Datasets\terms_KO.zip'
-csv_file_name_KO = 'Datasets\terms_KO.csv'
-zip_file_path_traits = 'Datasets\reducedDataset.zip'
-csv_file_name_traits = 'Datasets\reducedDataset.csv'
-
-# Read and preprocess KO terms
-with zipfile.ZipFile(zip_file_path_KO, 'r') as zip_ref:
-    with zip_ref.open(csv_file_name_KO) as file:
-        KOs = pd.read_csv(file, index_col=0)
-
-KOs['value'] = 1
-X_terms = KOs.pivot_table(index='key', columns='KO', values='value', fill_value=0)
-
-# Apply VarianceThreshold to remove constant features
-selector = VarianceThreshold(threshold=0.01)
-X_filtered = selector.fit_transform(X_terms)
-X_filtered_df = pd.DataFrame(X_filtered, index=X_terms.index, columns=X_terms.columns[selector.get_support()])
-
-# Optionally, inspect the variance of features to confirm removal
-feature_variances = X_filtered_df.var()
-print(f"Features with zero variance after filtering: {sum(feature_variances == 0)}")
-
-# Read and preprocess traits data
-with zipfile.ZipFile(zip_file_path_traits, 'r') as zip_ref:
-    with zip_ref.open(csv_file_name_traits) as file:
-        traits = pd.read_csv(file, sep=';')
-traits['oxygen'] = traits['oxygen'].str.lower()
-
-# Mapping of oxygen levels to a simplified classification
-oxygen_mapping = {
-    'aerobic': 'aerobic',
-    'aerotolerant': 'aerobic',
-    'microaerophilic': 'aerobic',
-    'obligate aerobic': 'aerobic',
-    'anaerobic': 'anaerobic',
-    'obligate anaerobic': 'anaerobic',
-    'conflict': 'aerobic',  
-    'facultative': 'aerobic'  
-}
-traits['oxygen'] = traits['oxygen'].map(oxygen_mapping)
-
-# Handling missing values and aggregating by key
-y = traits.dropna(subset=['oxygen']).groupby('key').agg({'oxygen': lambda x: x.value_counts().index[0]})
-
-# Find common keys after removing missing values
-common_keys = X_filtered_df.index.intersection(y.index)
-
-# Align X (features) and Y (labels) based on common keys
-X_aligned = X_filtered_df.loc[common_keys]
-Y_aligned = y.loc[common_keys].values.ravel()
-
-# Ensures X_aligned and Y_aligned are aligned
-assert X_aligned.shape[0] == len(Y_aligned), "X and Y are not aligned"
+    def evaluate_model(self):
+        # Calculate metrics, plot confusion matrix, etc.
+        pass
