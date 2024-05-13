0adf29dfad7c8c92c34f2386813032d6b87326b2
Ella <eliahmathias@gmail.com>
2024-05-13 14:36:05 +0200

    Started fixing OOP stuff

--- Data_Feature/Object_oriented_dataprocess.py
+++ Data_Feature/Object_oriented_dataprocess.py
@@ -1,21 +1,28 @@
-
-import numpy as np
 import sys
+sys.path.append('../Data_Feature')
+sys.path.append('../Datasets')
+print(sys.path)
+import numpy as np
 import os
+import seaborn as sns
+from scipy.stats import pearsonr
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.transforms as transforms
+from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, StratifiedKFold
-from sklearn.naive_bayes import MultinomialNB, GaussianNB # or GaussianNB if your data is normalized and continuous
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, precision_score, recall_score, f1_score, ConfusionMatrixDisplay, make_scorer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB # or GaussianNB if your data is normalized and continuous
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, precision_score, recall_score, f1_score, ConfusionMatrixDisplay, make_scorer, matthews_corrcoef
 from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_classif
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder
-import zipfile
+from sklearn.preprocessing import LabelEncoder 
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.utils import resample
+import zipfile 
 from bioservices import KEGG
-sys.path.append('../Datasets')
-
+from K_func import translate_ko_terms
 
 class DataProcessor:
     def __init__(self, terms_zip_path, terms_csv_path, traits_zip_path, traits_csv_path):
@@ -39,7 +46,7 @@ class DataProcessor:
 
 
 class KOProcessor(DataProcessor):
-    def preprocess_terms(self, terms_data):
+    def KO_preprocess_terms(self, terms_data):
 
         terms_data['value'] = 1
         X_terms = terms_data.pivot_table(index='key', columns='KO', values='value', fill_value=0)
@@ -50,7 +57,7 @@ class KOProcessor(DataProcessor):
         X_filtered_df = pd.DataFrame(X_filtered, index=X_terms.index, columns=X_terms.columns[selector.get_support()])
         return X_filtered_df
 
-    def preprocess_traits_oxygen(self, traits_data):
+    def KO_preprocess_traits_oxygen(self, traits_data):
         traits_data['oxygen'] = traits_data['oxygen'].str.lower()
         traits_data['oxygen'] = traits_data['oxygen'].map({
             'aerobic': 'aerobic',
@@ -67,21 +74,41 @@ class KOProcessor(DataProcessor):
 
 
 class GOProcessor(DataProcessor):
-    def preprocess_terms(self, terms_data):
 
-        return X_filtered_df
-    def preprocess_traits(self, traits_data):
-        
+    def preprocess_terms(self, terms_data): 
+
+        terms_data["value"] = 1
+        X_terms = terms_data.pivot_table(index = "key", columns = "GO", values = "value", fill_value=0)
+
+        # Variance threshold for feature removal
+        selector = VarianceThreshold(threshold=0.01)
+        X_filtered = selector.fit_transform(X_terms)
+        X = pd.DataFrame(X_filtered, index=X_terms.index, columns= X_terms.columns[selector.get_support()])
+        return X
+    
+
+    def preprocess_traits_oxygen(self, traits_data):
+        traits_data['oxygen'] = traits_data['oxygen'].str.lower()
+        traits_data['oxygen'] = traits_data['oxygen'].map({
+            'aerobic': 'aerobic',
+            'aerotolerant': 'aerobic',
+            'microaerophilic': 'aerobic',
+            'obligate aerobic': 'aerobic',
+            'anaerobic': 'anaerobic',
+            'obligate anaerobic': 'anaerobic',
+            'conflict': 'aerobic',  
+            'facultative': 'aerobic'  
+        })
+        y = traits_data.dropna(subset=['oxygen']).groupby('key').agg({'oxygen': lambda x: x.value_counts().index[0]})
         return y
 
-
-class COGsProcessor(DataProcessor):
-    def preprocess_terms(self, terms_data):
+'''class COGsProcessor(DataProcessor):
+    #def preprocess_terms(self, terms_data):
         return X_filtered_df
     
     def preprocess_traits(self, traits_data):
        return y
-    
+    '''
 
 
 
