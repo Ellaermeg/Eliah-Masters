95e11e5403ea65cb08e602f2df77cefa55cbe729
Bruh <eliahmathias@gmail.com>
2024-11-30 19:10:46 +0100

    Think i fixed single pipeline

--- Data_processing/data_processing.py
+++ Data_processing/data_processing.py
@@ -7,23 +7,61 @@ print(os.getcwd())
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.preprocessing import LabelEncoder
 
+class TraitManager:
+    def __init__(self):
+        self.trait_mappings = {
+            'gram': {
+                'mapping': {'positive': 'positive', 'negative': 'negative', 'nan': 'nan'},
+                'default_value': 'unknown'
+            },
+            'oxygen': {
+                'mapping': {
+                    'aerobic': 'aerobic', 'aerotolerant': 'aerobic', 'microaerophilic': 'aerobic',
+                    'obligate aerobic': 'aerobic', 'anaerobic': 'anaerobic', 'obligate anaerobic': 'anaerobic',
+                    'conflict': 'aerobic', 'facultative': 'aerobic'
+                },
+                'default_value': 'unknown'
+            },
+            'trophy': {
+                'levels': ['photo', 'chemo', 'litho', 'hetero', 'organo', 'auto']
+            }
+        }
+
+    def preprocess_trait(self, traits_data, trait_column):
+        if trait_column in self.trait_mappings:
+            if 'mapping' in self.trait_mappings[trait_column]:
+                mapping = self.trait_mappings[trait_column]['mapping']
+                traits_data[trait_column] = traits_data[trait_column].map(mapping).fillna(self.trait_mappings[trait_column]['default_value'])
+            elif 'levels' in self.trait_mappings[trait_column]:
+                trophic_levels = self.trait_mappings[trait_column]['levels']
+                binary_labels = {}
+                for level in trophic_levels:
+                    binary_labels[level] = traits_data[trait_column].apply(lambda x: 1 if level in x.lower() else 0)
+                return pd.DataFrame(binary_labels, index=traits_data['key'])
+        else:
+            print(f"Warning: Trait '{trait_column}' is not defined in trait mappings.")
+        return traits_data
+
 class DataProcessor:
-    def __init__(self, terms_zip_path, terms_csv_path, traits_reduced_zip_path, traits_reduced_csv_path, traits_assembled_zip_path=None, traits_assembled_csv_path=None):
+    def __init__(self, terms_zip_path=None, terms_csv_path=None, traits_reduced_zip_path=None, traits_reduced_csv_path=None, traits_assembled_zip_path=None, traits_assembled_csv_path=None):
         self.terms_zip_path = terms_zip_path
         self.terms_csv_path = terms_csv_path
         self.traits_reduced_zip_path = traits_reduced_zip_path
         self.traits_reduced_csv_path = traits_reduced_csv_path
         self.traits_assembled_zip_path = traits_assembled_zip_path
         self.traits_assembled_csv_path = traits_assembled_csv_path
+        self.trait_manager = TraitManager()
 
     def load_data_from_zip(self, zip_path, csv_path):
+        if not zip_path or not csv_path:
+            print("Error: Zip path or CSV path not provided.")
+            return None
+
         if not os.path.exists(zip_path):
             print(f"Error: The file {zip_path} does not exist.")
             return None
 
         try:
-            print("THIS IS CSV PATH", csv_path)
-            print("THIS IS ZIP PATH", zip_path)
             with zipfile.ZipFile(zip_path, 'r') as zip_ref:
                 with zip_ref.open(csv_path) as file:
                     data = pd.read_csv(file, sep=None, engine="python")
@@ -34,7 +72,6 @@ class DataProcessor:
                 data.columns = data.columns.str.replace('^\ufeff', '', regex=True)
                 print("Data loaded successfully:")
                 print(data.head())
-                print(data.columns)
             return data
         except zipfile.BadZipFile:
             print("Error: Bad Zip")
@@ -49,39 +86,17 @@ class DataProcessor:
 
     def load_reduced_traits_data(self):
         """Loads reduced traits data, which is the primary dataset."""
-        reduced_traits_data = self.load_data_from_zip(self.traits_reduced_zip_path, self.traits_reduced_csv_path)
-        if reduced_traits_data is not None:
-            # Strip whitespace from column names
-            reduced_traits_data.columns = reduced_traits_data.columns.str.strip()
-            # Convert all column names to lowercase
-            reduced_traits_data.columns = reduced_traits_data.columns.str.lower()
-            print("Columns in reduced traits data after processing:", reduced_traits_data.columns.tolist())
-        return reduced_traits_data
+        return self.load_data_from_zip(self.traits_reduced_zip_path, self.traits_reduced_csv_path)
 
     def load_assembled_traits_data(self):
         """Loads assembled traits data if assembled dataset paths are provided."""
-        if self.traits_assembled_zip_path and self.traits_assembled_csv_path:
-            assembled_traits_data = self.load_data_from_zip(self.traits_assembled_zip_path, self.traits_assembled_csv_path)
-            if assembled_traits_data is not None:
-                # Strip whitespace from column names
-                assembled_traits_data.columns = assembled_traits_data.columns.str.strip()
-                # Convert all column names to lowercase
-                assembled_traits_data.columns = assembled_traits_data.columns.str.lower()
-                print("Columns in assembled traits data after processing:", assembled_traits_data.columns.tolist())
-            return assembled_traits_data
-        else:
-            return None
+        return self.load_data_from_zip(self.traits_assembled_zip_path, self.traits_assembled_csv_path)
 
     def preprocess_features(self, terms_data, column_name):
         """Processes terms data to create a feature matrix and remove low variance features."""
         terms_data['value'] = 1
         X_terms = terms_data.pivot_table(index='key', columns=column_name, values='value', fill_value=0)
-
-        # Apply Variance Threshold
-        selector = VarianceThreshold(threshold=0.01)
-        X_filtered = selector.fit_transform(X_terms)
-        X_filtered_df = pd.DataFrame(X_filtered, index=X_terms.index, columns=X_terms.columns[selector.get_support()])
-        return X_filtered_df
+        return X_terms
 
     def align_data(self, X, y):
         """Aligns features and labels based on common keys."""
@@ -125,6 +140,10 @@ class KOProcessor(DataProcessor):
         - trait_column: The column of the trait that we want to process.
         - use_assembled_if_missing: Boolean to decide whether to use the assembled dataset if the trait column is missing.
         """
+        if reduced_traits_data is None:
+            print("Error: Reduced traits data is not available.")
+            return None
+
         required_columns = ['key', 'speciesstrain', 'speciesstraincomp']
         missing_columns = [col for col in required_columns if col not in reduced_traits_data.columns]
 
@@ -133,13 +152,11 @@ class KOProcessor(DataProcessor):
             return None
 
         if trait_column not in reduced_traits_data.columns and use_assembled_if_missing:
-            # Load assembled traits data if the trait is missing in the reduced dataset
             traits_assembled = self.load_assembled_traits_data()
             if traits_assembled is not None:
-                # Check if necessary columns exist in assembled data
                 required_assembled_columns = [trait_column, 'speciesstraincomp', 'database']
                 missing_assembled_columns = [col for col in required_assembled_columns if col not in traits_assembled.columns]
-                
+
                 if missing_assembled_columns:
                     print(f"Error: Missing columns {missing_assembled_columns} in the assembled traits data.")
                     return None
@@ -151,33 +168,16 @@ class KOProcessor(DataProcessor):
                     on='speciesstraincomp',
                     how='inner'
                 )
-                # Optional: Save the merged traits to a CSV file for future use
-                reduced_traits_data.to_csv('traits_indexed.csv', index=False)
 
-        # Process the given trait column if available
         if trait_column in reduced_traits_data.columns:
             reduced_traits_data = reduced_traits_data.dropna(subset=[trait_column])
-            reduced_traits_data[trait_column] = reduced_traits_data[trait_column].str.lower()
-
-            # Define trophic levels
-            trophic_levels = ['photo', 'chemo', 'litho', 'hetero', 'organo', 'auto']
-
-            gram_staining
-
-            # Create binary labels for each trophic level
-            binary_labels = {}
-            for trophy in trophic_levels:
-                binary_labels[trophy] = reduced_traits_data[trait_column].apply(lambda x: 1 if trophy in x else 0)
-
-            y = pd.DataFrame(binary_labels)
-            return y
-
+            return self.trait_manager.preprocess_trait(reduced_traits_data, trait_column)
         else:
             print(f"Error: Trait column '{trait_column}' not found in the reduced traits data after merging.")
             return None
 
 class GOProcessor(DataProcessor):
-    def preprocess_terms(self, terms_data): 
+    def preprocess_terms(self, terms_data):
         """Processes GO terms using the generic feature preprocessing method."""
         return self.preprocess_features(terms_data, 'GO')
 
@@ -191,15 +191,12 @@ if __name__ == "__main__":
     traits_assembled_zip_path = 'C:/Users/eliah/Documents/Master/Eliah-Masters/Datasets/assembledDataset.zip'
     traits_assembled_csv_path = 'assembledDataset.csv'
 
-    # Print current working directory
-    print("Current Working Directory:", os.getcwd())
-
     # Instantiate KOProcessor and load data
     processor = KOProcessor(
-        terms_zip_path, 
-        terms_csv_path, 
-        traits_reduced_zip_path, 
-        traits_reduced_csv_path, 
+        terms_zip_path=terms_zip_path, 
+        terms_csv_path=terms_csv_path, 
+        traits_reduced_zip_path=traits_reduced_zip_path, 
+        traits_reduced_csv_path=traits_reduced_csv_path, 
         traits_assembled_zip_path=traits_assembled_zip_path, 
         traits_assembled_csv_path=traits_assembled_csv_path
     )
@@ -213,7 +210,7 @@ if __name__ == "__main__":
     if reduced_traits_data is None:
         raise FileNotFoundError("Reduced traits data could not be loaded. Please check the file paths.")
 
-    # Preprocess KO terms and trophy traits
+    # Preprocess KO terms and traits (trophy, gram, oxygen)
     X_terms = processor.preprocess_terms(ko_terms)
     y_traits = processor.preprocess_traits(reduced_traits_data, trait_column='trophy', use_assembled_if_missing=True)
 
@@ -224,6 +221,10 @@ if __name__ == "__main__":
     # Align features and labels
     X_aligned, y_aligned = processor.align_data(X_terms, y_traits)
 
+    # Feature Selection: Variance Threshold
+    selector = VarianceThreshold(threshold=0.04)
+    X_aligned = selector.fit_transform(X_aligned)
+
     # Check results
-    print(f"Aligned X shape: {X_aligned.shape}")
+    print(f"Aligned X shape after feature selection: {X_aligned.shape}")
     print(f"Aligned Y shape: {y_aligned.shape}")
